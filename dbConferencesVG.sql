ceefda6f0c6fa1839eb606777ed1316c7c84b64e
-TR_Lab01 <abel.huamani@vallegrande.edu.pe>
2022-09-13 17:01:32 -0500

    actualizacion

---
+++ dbConferencesVG.sql
@@ -0,0 +1,156 @@
+CREATE DATABASE dbConferencesVG
+Go
+
+set language spanish;
+
+CREATE TABLE PARTICIPANTE
+(
+    IDPAR int NOT NULL IDENTITY (1,1),
+    NOMPAR VARCHAR(80) NOT NULL,
+    APEPAR VARCHAR(80)NOT NULL,
+    DNIPAR CHAR(8) NOT NULL,
+	TIPPAR CHAR(1) NOT NULL,
+	CELPAR CHAR(9) NOT NULL,
+	EMAPAR VARCHAR(90) NOT NULL,
+	DIRPAR VARCHAR(90) NOT NULL,
+	ESTPAR CHAR(1) DEFAULT 'A',
+	CONSTRAINT PARTICIPANTE_PK PRIMARY KEY (IDPAR) 
+	)
+GO
+
+CREATE TABLE PONENTE
+(
+    CODPON CHAR(5) NOT NULL,
+	NOMPON VARCHAR(60)NOT NULL,
+	APEPON VARCHAR(80)NOT NULL,
+	CELPONN CHAR(9)NOT NULL,
+	DNIPON CHAR(8)NOT NULL,
+	EMAPON VARCHAR(90)NOT NULL,
+	DIRPON VARCHAR(90)NOT NULL,
+	CONSTRAINT PONENTE_PK PRIMARY KEY (CODPON) 
+	)
+GO
+
+CREATE TABLE  CONFERENCIA
+(
+    CODCONF CHAR(5) NOT NULL,
+	TEMCONF VARCHAR(90) NOT NULL,
+	FECCONF DATE NOT NULL,
+	CODPON CHAR(5) NOT NULL,
+	CONSTRAINT CONFERENCIA_PK PRIMARY KEY (CODCONF) 
+	)
+GO
+
+CREATE TABLE  REGISTRO
+(
+    CODREG CHAR(5) NOT NULL,
+	IDPAR INT,
+	FECREG DATE NOT NULL,
+	CERTREG CHAR(1) NOT NULL,
+	CONSTRAINT REGISTRO_PK PRIMARY KEY (CODREG) 
+	)
+GO
+
+CREATE TABLE  REGISTRODETALLE
+(
+    IDREGDET INT NOT NULL,
+	CODREG	 CHAR(5) NOT NULL,
+	CODCONF  CHAR(5) NOT NULL,
+	CANTPART INT NOT NULL,
+	CONSTRAINT REGISTRODETALLE_PK PRIMARY KEY (IDREGDET) 
+	)
+GO
+
+----ALTER TABLE REGISTRO_PARTICIPANTE
+ALTER TABLE REGISTRO ADD CONSTRAINT REGISTRO_PARTICIPANTE
+    FOREIGN KEY (IDPAR)
+    REFERENCES PARTICIPANTE (IDPAR);
+
+----ALTER TABLE REGISTRODETALLE_REGISTRO
+ALTER TABLE REGISTRODETALLE ADD CONSTRAINT REGISTRODETALLE_REGISTRO
+    FOREIGN KEY (CODREG)
+    REFERENCES REGISTRO (CODREG);
+
+----ALTER TABLE REGISTRODETALLE_CONFERENCIA
+ALTER TABLE REGISTRODETALLE ADD CONSTRAINT REGISTRODETALLE_CONFERENCIA
+    FOREIGN KEY (CODCONF)
+    REFERENCES CONFERENCIA (CODCONF);
+
+----ALTER TABLE REGISTRODETALLE_CONFERENCIA
+ALTER TABLE CONFERENCIA ADD CONSTRAINT CONFERENCIA_PONENTE
+    FOREIGN KEY (CODPON)
+    REFERENCES PONENTE (CODPON);
+
+---INSERT INTO
+INSERT INTO PARTICIPANTE
+(NOMPAR, APEPAR, DNIPAR, TIPPAR, CELPAR, EMAPAR, DIRPAR)
+VALUES
+('Juan', 'Campos Perez', '40255133', '1','986512478','juan.campos@vallegrande.edu.pe','AV.Miraflores'),
+('Sofia', 'Solano Avila', '64978531', '1','974815258','safia.solano@vallegrande.edu.pe','jr.Huallaga'),
+('Maria', 'Rosales Guerra ', '15925874', '1','986532147','maria.rosales@vallegrande.edu.pe','Calle girasoles'),
+('Marcos ', 'Alarcon Hermosa', '48781512', '2','986532147','marcos.alarcon@vallegrande.edu.pe','C'),
+('Martin', 'Samán Arata','84152631', '2' ,' ' , 'martin.sama@vallegrande.edu.pe','Jr. La Union'),
+('Jose',	'Quispe Luyo',	'48161937',  '2', '978415321','jose.quispse@vallegrande.edu.pe','Calle Abancay'),
+('Claudia',	'Barraza Carrasco',	'78452596','3',' ','cbarraza@vallegrande.edu.pe','Jr. Las cardenias'),
+('Jhoana',	'Bendezú Anccasi',	'74321564','3',' ','jbendezu@vallegrande.edu.pe',''),
+('Mario',	'Acosta Palomino',	'15326499','3','931764521','mario.acosta@outllok.com','Av.Miraflores')
+GO
+
+INSERT INTO PONENTE
+(CODPON, NOMPON, APEPON, CELPONN, DNIPON, EMAPON, DIRPON)
+VALUES
+('P0001', 'Alberto ', 'Corrales losano', '','15263798','alberto.corrales@yahoo.com','Calle los Portales'),
+('P0002', 'Juana ', 'Sanches Ortego', '974815258','13256497','juana.sanches@outlook.com','Av. La Libertad'),
+('P0003', 'Javier  ', 'Nakasone Vila', '995236147','15953575','javier.nakasone@gmail.com','Jr. San Martin'),
+('P0004', 'Sonia', 'Huayta Medina', ' ','12657814','sonia.huayta@gmail.com','Av. Las Gardenias'),
+('P0005', 'Fabiano', 'Carrion Avila', ' ','12233647',' ','Jr. Huanacayo')
+
+GO
+
+INSERT INTO CONFERENCIA
+(CODCONF, TEMCONF, FECCONF, CODPON)
+VALUES
+('C0001', 'Gestion de datos con MySQL ', '15/7/2022', 'P0001'),
+('C0002', 'Joins paso a paso ', '16/7/2022', 'P0002'),
+('C0003', 'Consultas con parametros ', '17/7/2022', 'P0003'),
+('C0004', 'Administración base de datos MySQL ', '16/7/2022', 'P0002'),
+('C0005', 'Gestión de backups de Base de Datos ', '15/7/2022', 'P0004')
+GO
+
+INSERT INTO REGISTRO 
+(CODREG, IDPAR, FECREG, CERTREG)
+VALUES
+('R0001', '1 ', '', 'S'),
+('R0002', '3 ', '', 'S'),
+('R0003', '4', '', 'S'),
+('R0004', '7', '', 'S'),
+('R0005', '2', '', 'N'),
+('R0006', '5', '', 'N'),
+('R0007', '6', '', 'N'),
+('R0008', '9', '', 'N')
+GO
+
+INSERT INTO REGISTRODETALLE 
+(IDREGDET, CODREG, CODCONF, CANTPART)
+VALUES
+('1', 'R0001', 'C0001', '2'),
+('2', 'R0002', 'C0002', '1'),
+('3', 'R0001', 'C0001', '4'),
+('4', 'R0002', 'C0003', '1'),
+('5', 'R0003', 'C0004', '1'),
+('6', 'R0004', 'C0005', '2'),
+('7', 'R0005', 'C0005', '3'),
+('8', 'R0005', 'C0003', '2'),
+('9', 'R0006', 'C0002', '3'),
+('10','R0006', 'C0003', '4'),
+('11','R0007', 'C0005', '2'),
+('12','R0007', 'C0003', '6'),
+('13','R0008', 'C0005', '1')
+GO
+
+SELECT * FROM CONFERENCIA
+SELECT * FROM REGISTRO
+SELECT * FROM REGISTRODETALLE
+SELECT * FROM PARTICIPANTE
+SELECT * FROM PONENTE
+SELECT * FROM CONFERENCIA
